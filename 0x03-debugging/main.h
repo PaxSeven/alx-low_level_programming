ddc1d533a7044c6d2b996345ee9a022fe8cdea83
PaxSeven <peacesimon50@gmail.com>
2023-03-04 17:14:34 -0800

    update debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positiv_or_negative(int i);
+int largest_number(int a; int b; int c);
+void print_remaining_days(int month; int day; int year);
+int cinvert_day(inth month;int day;
+
+#endif
